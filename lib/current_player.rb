3b6a45b61e7c2bb7b66cfbdcb142e9e6cf936c59
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-07 17:22:16 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,11 @@
 def turn_count(board)
   counter = 0
   board.each do |spot|
-    counter += (spot != " " && spot !="") ? 1 : 0
-    # if (spot != " " && spot !="")
-    #   counter += 1
-    # end
+    counter += (spot != " " && spot !="") ? 1 : 0    
   end
   return counter
+end
+
+def current_player(board)
+  return turn_count % 2 > 0 ? " X " : " O "
 end
